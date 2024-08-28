b94eefab87817020249438887be7ca29c6b9a9d0
musmanirfan <usmanmala356@gmail.com>
2024-08-29 03:27:45 +0500

    Add functionality added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import AllStudents from "./allStudents/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+
+
+      <Link href={"/addStudents"}><button>Add Student</button></Link>
+
       <AllStudents />
     </>
   );
